3f581faab179f0b7dc2ffd7f1a9f634386199da4
JB Gosselin <1536838+jbgosselin@users.noreply.github.com>
2023-11-11 14:24:29 -0500

    Add item.yaml

--- build-charts.py
+++ build-charts.py
@@ -4,20 +4,13 @@ from glob import glob
 from os import path
 import yaml
 import json
-from shutil import copytree
+import shutil
 from datetime import datetime
 import markdown
 
 catalogPath = "catalog.json"
 sourceFolder = "src"
 trainName = "charts"
-maintainers = [
-    {
-        "email": "1536838+jbgosselin@users.noreply.github.com",
-        "name": "JB Gosselin",
-        "url": "https://github.com/jbgosselin",
-    },
-]
 
 def main():
     lastUpdate = datetime.utcnow().isoformat()
@@ -39,12 +32,19 @@ def main():
         with open(readmeFilePath) as openedFile:
             readmeContent = markdown.markdown(openedFile.read())
 
+        itemFileSrcPath = path.join(chartFolder, "item.yaml")
+        with open(itemFileSrcPath) as openedFile:
+            itemInfo = yaml.safe_load(openedFile)
+
         destFolder = path.join(trainName, appName, chartFile["version"])
-        copytree(chartFolder, destFolder, dirs_exist_ok=True)
+        shutil.copytree(chartFolder, destFolder, dirs_exist_ok=True)
+
+        itemFileDestPath = path.join(trainName, appName, "item.yaml")
+        shutil.copyfile(itemFileSrcPath, itemFileDestPath)
         
         catalog[trainName][appName] = {
             "app_readme": readmeContent,
-            "categories": chartFile["categories"],
+            "categories": itemInfo["categories"],
             "description": chartFile["description"],
             "healthy": True,
             "healthy_error": None,
@@ -56,12 +56,12 @@ def main():
             "last_update": lastUpdate,
             "name": appName,
             "recommended": False,
-            "title": chartFile["title"],
-            "maintainers": maintainers,
-            "tags": chartFile["keywords"],
-            "screenshots": [],
+            "title": chartFile["annotations"]["title"],
+            "maintainers": chartFile["maintainers"],
+            "tags": itemInfo["tags"],
+            "screenshots": itemInfo["screenshots"],
             "sources": chartFile["sources"],
-            "icon_url": chartFile["icon"]
+            "icon_url": itemInfo["icon_url"]
         }
     
     with open(catalogPath, mode="w") as catalogFile:
